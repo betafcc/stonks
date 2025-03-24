eb41e3f5e88d834ae6dd709b6d438918934e113a
betafcc <betafcc@gmail.com>
2025-03-24 20:37:12 +0100

    adds bets schema

--- backend/db/schema.ts
+++ backend/db/schema.ts
@@ -5,7 +5,9 @@ import {
   serial,
   boolean,
   timestamp,
+  pgEnum,
   integer,
+  numeric,
 } from 'drizzle-orm/pg-core'
 import { relations } from 'drizzle-orm'
 
@@ -24,3 +26,29 @@ export const users = pgTable('users', {
   //   .defaultNow()
   //   .$onUpdate(() => new Date()),
 })
+
+export const usersRelations = relations(users, ({ many }) => ({
+  bets: many(bets),
+}))
+
+export const directionEnum = pgEnum('direction', ['up', 'down'])
+
+export const bets = pgTable('bets', {
+  id: serial('id').primaryKey(),
+  userId: integer('user_id')
+    .notNull()
+    .references(() => users.id, { onDelete: 'cascade' }),
+  symbol: varchar('symbol', { length: 64 }).notNull(),
+  direction: directionEnum('direction'),
+  initial: numeric('initial', { precision: 20, scale: 2 }).notNull(),
+  initialTime: timestamp('initial_time').notNull(), // no default
+  final: numeric('final', { precision: 20, scale: 2 }),
+  finalTime: timestamp('final_time'),
+})
+
+export const betsRelations = relations(bets, ({ one, many }) => ({
+  user: one(users, {
+    fields: [bets.userId],
+    references: [users.id],
+  }),
+}))
