d5d1a617b204f9f3a2daecbee2751ed53082d336
betafcc <betafcc@gmail.com>
2025-03-26 22:31:51 +0100

    removes logging; fixes standalone server

--- backend/standalone-server.ts
+++ backend/standalone-server.ts
@@ -1,13 +1,22 @@
+import jwt, { type JwtPayload } from 'jsonwebtoken'
 import { initTRPC } from '@trpc/server'
 import { createHTTPServer } from '@trpc/server/adapters/standalone'
 import cors from 'cors'
-import { router } from './router'
+import { router, type Context } from './router'
+import { getUserById } from './service'
 
 createHTTPServer({
   middleware: cors(),
   router: router,
-  createContext() {
-    console.log('context 3')
+  createContext: async ({ req, res }): Promise<Context> => {
+    const token = req.headers.authorization?.split(' ')[1]
+    if (token) {
+      const decoded = jwt.verify(token, 'secret') as JwtPayload
+      return {
+        user: await getUserById(decoded.id),
+      }
+    }
+
     return {}
   },
 }).listen(3001)
