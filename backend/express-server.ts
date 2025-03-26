5ffbddc117520435510cd461f6564ca2cbcb0512
betafcc <betafcc@gmail.com>
2025-03-26 22:18:53 +0100

    fix trpc cors

--- backend/express-server.ts
+++ backend/express-server.ts
@@ -7,9 +7,12 @@ import express from 'express'
 import { router, type Context } from './router'
 import { getUserById } from './service'
 
-express()
-  .use(morgan('tiny'))
+export const app = express()
   .use(cors())
+  .use(morgan('tiny'))
+  .get('/health-check', (_, res) => {
+    res.send('ok')
+  })
   .use(
     trpcExpress.createExpressMiddleware({
       router,
@@ -24,6 +27,13 @@ express()
 
         return {}
       },
+      responseMeta() {
+        return {
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET,PUT,DELETE',
+          },
+        }
+      },
     }),
   )
-  .listen(3001)
