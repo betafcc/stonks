6f18f08446c143ea95fc039b881f0853b69532b6
betafcc <betafcc@gmail.com>
2025-03-26 22:48:11 +0100

    hardcodes api endpoind on fe

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -4,7 +4,7 @@ import type { Router } from '../../backend/router'
 export const api = createTRPCClient<Router>({
   links: [
     httpBatchLink({
-      url: process.env.API_URL!,
+      url: 'https://stonks-2usr.onrender.com',
       async headers() {
         const token = window?.localStorage.getItem('token')
         if (token)
